1427467ab92a4e74c3a004bcad257a79cf89462e
Pham Quang Huy <huypham060398@gmail.com>
2017-08-23 19:43:58 +0300

    23/8/2017

--- App/comm.h
+++ App/comm.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-#define SERVER_URL "http://www.cc.puv.fi/~e1601124/APPDEV/APP_save.php"
+#define SERVER_URL "http:/pqhuy98.hopto.org/APPDEV/APP_save.php"
 #define DEBUG_URL "localhost/APPDEV/APP_save.php"
 
 //Send file wav and a string of decibel values to url by HTTP POST
